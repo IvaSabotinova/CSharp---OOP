380da8dd5e87d23ba4019a159b14336a1e6842dc
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-04-14 12:29:57 +0300

    Add files via upload

---
+++ ExamPreparation/01.Structure_Skeleton_16_08_2020(OnlineShop)/OnlineShop/Models/Products/Computers/Laptop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.ComputerModels
+{
+public   class Laptop: Computer
+    {
+        public Laptop(int id, string manufacturer, string model, decimal price) : base(id, manufacturer, model, price, 10)
+        {
+        }
+    }
+}
