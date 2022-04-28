df5c470aacec8ea2f1bb492c8009361d4e31746c
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-04-28 14:07:32 +0300

    Add files via upload

---
+++ ExamPreparation/01. Business Logic_Skeleton_21_04_2019(H.A.D.)/HAD/Entities/Heroes/Assassin.cs
@@ -0,0 +1,9 @@
+﻿namespace HAD.Entities.Heroes
+{
+    public class Assassin : BaseHero
+    {
+        public Assassin(string name) : base(name, 25, 100, 15, 150, 300)
+        {
+        }
+    }
+}
---
+++ ExamPreparation/01. Business Logic_Skeleton_21_04_2019(H.A.D.)/HAD/Entities/Heroes/Wizard.cs
@@ -0,0 +1,9 @@
+﻿namespace HAD.Entities.Heroes
+{
+    public class Wizard : BaseHero
+    {
+        public Wizard(string name) : base(name, 25, 25, 100, 100, 250)
+        {
+        }
+    }
+}
