9f340f6541fb7a827331bcef7fab1f75836b6f6d
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-04-28 16:24:12 +0300

    Add files via upload

---
+++ ExamPreparation/01. Business Logic_Skeleton_21_04_2019(H.A.D.)/HAD.Tests/HeroInventoryTests.cs
@@ -0,0 +1,53 @@
+using HAD.Contracts;
+using HAD.Entities.Items;
+using HAD.Entities.Miscellaneous;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HAD.Tests
+{
+
+    public class HeroInventoryTests
+    {
+        private HeroInventory heroInventory;
+        private CommonItem commonItem1;
+        private CommonItem commonItem2;
+        private IRecipe recipeitem1;
+        private IRecipe recipeitem2;
+        [SetUp]
+        public void Setup()
+        {
+            heroInventory = new HeroInventory();
+            commonItem1 = new CommonItem("aaa", 5, 5, 10, 10, 10);
+            commonItem2 = new CommonItem("bbb", 10, 10, 5, 5, 5);
+            List<string> recipe1 = new List<string>() { "aaa" };
+            List<string> recipe2 = new List<string>() { "bbb" };
+            recipeitem1 = new RecipeItem("aaa11", 15, 15, 30, 30, 30, recipe1);
+            recipeitem2 = new RecipeItem("bbb11", 7, 9, 9, 35, 35, recipe2);
+           
+        }
+        [Test]
+        public void CheckAddNewItemsToCommonItems()
+        {
+            heroInventory.AddCommonItem(commonItem1);
+            heroInventory.AddCommonItem(commonItem2);
+
+            Assert.AreEqual(2, heroInventory.CommonItems.Count);
+        }
+        [Test]
+        public void CheckAddNewRecipeItemsAndCombineRecipeMethods()
+        {
+            heroInventory.AddCommonItem(commonItem1);
+            heroInventory.AddCommonItem(commonItem2);
+            heroInventory.AddRecipeItem(recipeitem1);
+            heroInventory.AddRecipeItem(recipeitem2);
+            Assert.AreEqual(2, heroInventory.CommonItems.Count);
+            Assert.AreEqual(22, heroInventory.TotalStrengthBonus);
+            Assert.AreEqual(24, heroInventory.TotalAgilityBonus);
+            Assert.AreEqual(39, heroInventory.TotalIntelligenceBonus);
+            Assert.AreEqual(65, heroInventory.TotalHitPointsBonus);
+            Assert.AreEqual(65, heroInventory.TotalDamageBonus);
+
+        }
+    }
+}
---
+++ ExamPreparation/01. Business Logic_Skeleton_21_04_2019(H.A.D.)/HAD/Entities/Items/RecipeItem.cs
@@ -0,0 +1,17 @@
+﻿using HAD.Contracts;
+using System.Collections.Generic;
+
+
+namespace HAD.Entities.Items
+{
+    public class RecipeItem : BaseItem, IRecipe
+    {
+     
+        public RecipeItem(string name, long strengthBonus, long agilityBonus, long intelligenceBonus, long hitPointsBonus, long damageBonus, List<string> requiredItems) : base(name, strengthBonus, agilityBonus, intelligenceBonus, hitPointsBonus, damageBonus)
+        {
+            RequiredItems = requiredItems;
+        }
+
+        public IReadOnlyList<string> RequiredItems { get; private set; }
+    }
+}
